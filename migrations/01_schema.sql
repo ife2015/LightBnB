cfd1b863d944c38021ad507a5841836ab6be23ec
Ife Olaifa <ifeolaifa5@gmail.com>
2020-10-14 22:57:05 +0000

    update

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -15,16 +15,16 @@ CREATE TABLE users (
 
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
-  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE, 
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
 
   title VARCHAR(255) NOT NULL,
-  description TEXT, 
-  thumbnail_photo_url VARCHAR(255) NOT NULL, 
+  description TEXT,
+  thumbnail_photo_url VARCHAR(255) NOT NULL,
   cover_photo_url VARCHAR(255) NOT NULL,
-  cost_per_night INTEGER NOT NULL DEFAULT 0, 
-  parking_space INTEGER NOT NULL DEFAULT 0,
-  number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
-  number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
+  cost_per_night INTEGER  NOT NULL DEFAULT 0,
+  parking_spaces INTEGER  NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER  NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER  NOT NULL DEFAULT 0,
 
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
@@ -33,7 +33,7 @@ CREATE TABLE properties (
   post_code VARCHAR(255) NOT NULL,
 
   active BOOLEAN NOT NULL DEFAULT TRUE
-); 
+);
 
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
